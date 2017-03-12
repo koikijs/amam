c9428e45e37ff72079484794f403cbe4bd3fdf71
sideroad <sideroad.jp@gmail.com>
2017-03-12 12:22:12 +0900

    Add timeline component

--- src/containers/TimelinePage.jsx
+++ src/containers/TimelinePage.jsx
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { asyncConnect } from 'redux-connect';
-import { Datepicker, Chips } from 'koiki-ui';
+import { Datepicker, Chips, Timeline } from 'koiki-ui';
 import PersonsList from '../components/PersonsList';
 
 import styles from '../css/timeline-page.less';
@@ -35,10 +35,37 @@ const TimeLinePage = props => console.log(props) ||
           ]}
         />
       </div>
-      <div>
-        <PersonsList
-          items={props.person.items}
-        />
+      <div className={styles.container}>
+        <div className={styles.leftnav}>
+          <PersonsList
+            items={props.person.items}
+          />
+        </div>
+        <div className={styles.rightnav}>
+          <Timeline
+            offsetMinutes={510}
+            start="2017-02-18T15:00:00.000Z"
+            end="2017-02-25T14:59:59.999Z"
+            items={[
+              {
+                id: '111',
+                periods: [
+                  {
+                    id: '111-111',
+                    start: '2017-02-19T00:00:00.000Z',
+                    end: '2017-02-19T08:30:00.000Z',
+                    className: 'flight'
+                  }, {
+                    id: '111-222',
+                    start: '2017-02-20T00:00:00.000Z',
+                    end: '2017-02-20T08:30:00.000Z',
+                    className: ''
+                  }
+                ]
+              }
+            ]}
+          />
+        </div>
       </div>
     </div>
   </div>
